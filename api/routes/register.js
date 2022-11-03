faf06a7fa6c16215aa6b4a12ba088ddf569f9fac
rightdirection1 <lyubima.takova@gmail.com>
2022-11-03 16:55:55 +0200

    ...

--- api/routes/register.js
+++ api/routes/register.js
@@ -7,15 +7,16 @@ const jwt = require("jsonwebtoken");
 // REGISTER POST REQUEST
 router.post("/register", (req, res, next) => {
   const username = req.body.username;
-  const password = req.body.password; 
+  const password = req.body.password;
+  const email = req.body.email; 
   
  //Hashing the password
  // const hashedPassword = bcrypt.hashSync(password, bcrypt.genSaltSync());
 
   
   db.query(
-    "INSERT INTO users (username, password) VALUES (?,?)",
-    [username, password] 
+    "INSERT INTO users (username, password, email) VALUES (?,?,?)",
+    [username, password,email] 
   );
 });
 
